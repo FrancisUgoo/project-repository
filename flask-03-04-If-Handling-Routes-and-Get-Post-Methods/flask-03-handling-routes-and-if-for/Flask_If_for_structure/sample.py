73f6871ab7158d392d2a2d22964d6e9ffe9f5f59
Francis <francis.ikwuanusi@gmail.com>
2022-08-24 20:54:00 -0500

    This is our second hands-on

---
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/Flask_If_for_structure/sample.py
@@ -0,0 +1,12 @@
+A = 5
+
+if A > 30:
+    print("A is bigger than 30")
+elif 30 > A > 20:
+    print("A is between 30 and 20")
+else:
+    print("A is smaller than than 20")
+
+
+for name in ("ga","gh"):
+    print("goood "+name)
